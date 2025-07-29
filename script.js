1bf9dbbf4413c1498944e6351febcea4c8874b7d
MustaphaMouchtarik <110406056+MustaphaMouchtarik@users.noreply.github.com>
2025-07-29 19:57:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-Uppers=['A', 'B', 'C', 'D', 'E', 'F', 'G','H', 'I', 'J', 'K', 'L', 'M', 'N',
-    'O', 'P', 'Q', 'R', 'S', 'T', 'U','V', 'W', 'X', 'Y', 'Z'];
-lowers=['a', 'b', 'c', 'd', 'e', 'f', 'g','h', 'i', 'j', 'k', 'l', 'm', 'n',
-    'o', 'p', 'q', 'r', 's', 't', 'u','v', 'w', 'x', 'y', 'z'];
+Uppers=['A', 'B', 'C', 'D', 'E', 'F', 'G','H', 'I', 'J', 'K', 'L', 'M', 'N','O', 'P', 'Q', 'R', 'S', 'T', 'U','V', 'W', 'X', 'Y', 'Z'];
+lowers=['a', 'b', 'c', 'd', 'e', 'f', 'g','h', 'i', 'j', 'k', 'l', 'm', 'n','o', 'p', 'q', 'r', 's', 't', 'u','v', 'w', 'x', 'y', 'z'];
 numbers=['0','1','2','3','4','5','6','7','8','9'];
 symbols=["!","@","#","$","%","(",")","?","^"];
 function randomize(arr){
@@ -16,7 +14,7 @@ function generatePassword() {
     const includeSymbole = document.getElementById("Symbol").checked;
     let result = []
     if (!includeLower && !includeNumbers && !includeSymbole && !includeUpper){
-        window.alert("chose an includ nigga")
+        window.alert("include a character please")
     }
     for (i = 0 ; i<passlenght ; i++){
         let chosen=[];
